0c6127db064b74aa9c65c4d8735c690065a550f6
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-21 21:08:36 -0300

    up

--- atividade2_pag284.py
+++ atividade2_pag284.py
@@ -1,3 +1,11 @@
-for i in range(1,100):
-    if i is is_prime:
-        print(i)
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if not n%i:
+            return False
+    else:
+        return True
+for x in range(1,101):
+    if is_prime(x):
+        print(x)
